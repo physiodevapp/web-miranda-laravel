3b80f3f72734f29fe8fe4fdd5e124dc6fea2498e
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-09-25 12:07:50 +0200

    feat: add favicon

--- resources/views/components/miranda-layout.blade.php
+++ resources/views/components/miranda-layout.blade.php
@@ -8,6 +8,7 @@
     <!-- Global -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    <link rel="icon" href="{{ asset('images/H.svg') }}" type="image/svg+xml"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
